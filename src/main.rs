bd4cbbcc4ec723e5e153c1c3c7bd99dd00d508e1
root <root@FZ-Desktop>
2024-11-17 08:12:45 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::io::Write;
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+pub fn connection_handler(mut stream: TcpStream){
+    stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,12 +15,16 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                connection_handler(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+
+
 }
